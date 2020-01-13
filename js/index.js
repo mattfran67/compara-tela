2d3cbf0cb80731b4775987995477d4bd3f99f72f
Mateus Gonçalves Franco <mattfran67@gmail.com>
2020-01-12 23:30:12 -0300

    Adicionado informações das medidas dos monitores e melhorado um pouco o codigo

--- js/index.js
+++ js/index.js
@@ -1,24 +1,38 @@
+var aspectRatio = document.querySelectorAll('select');
+var input = document.querySelectorAll('input');
+var monitor = document.querySelectorAll('.monitor');
+var inform = document.querySelectorAll(".info");
+
 resolucao(27, 4, 3, 0);
-	resolucao(27, 4, 3, 1);
+resolucao(27, 4, 3, 1);
 
-	function aspect(i) {
-		var aspect = document.getElementsByTagName('select');
-		var input = document.getElementsByTagName('input');
-		var tamanho = aspect[i].value.split(":");
+function aspect(i) {
+	var tamanho = aspectRatio[i].value.split(":");
 
-		resolucao(input[i].value, tamanho[0], tamanho[1], i);
-	}
+	resolucao(input[i].value, tamanho[0], tamanho[1], i);
+}
 
-	function resolucao(pol, w, h, pos) {
-		var monitor = document.getElementsByClassName('monitor');
-		var soma_quad = w * w + h * h;
-		var tam = 15, alt, larg;
+function resolucao(pol, w, h, pos) {
+	var soma_quad = w * w + h * h;
+	var tam = 15, alt, larg;
 
-		larg = (pol * Math.sqrt(1 / soma_quad) * w) * tam;
-		alt = (pol * Math.sqrt(1 / soma_quad) * h) * tam;
+	larg = (pol * Math.sqrt(1 / soma_quad) * w) * tam;
+	alt = (pol * Math.sqrt(1 / soma_quad) * h) * tam;
 
-		monitor[pos].style.width = larg + "px";
-		monitor[pos].style.height = alt + "px";
-		monitor[pos].style.lineHeight = alt + "px";
-		monitor[pos].innerHTML = "Aspect Ratio " + w + ":" + h;
-	}
+	monitor[pos].style.width = larg + "px";
+	monitor[pos].style.height = alt + "px";
+	monitor[pos].style.lineHeight = alt + "px";
+	monitor[pos].innerHTML = "Aspect Ratio " + w + ":" + h;
+
+	info(larg / tam, alt / tam, pol, pos)
+}
+
+function info(larg, height, diag, pos) {
+	var largCent = (larg * 2.54).toFixed(2);
+	var heightCent = (height * 2.54).toFixed(2);
+	var diagCent = (diag * 2.54).toFixed(2);
+	inform[pos].innerHTML = 
+	"Largura: " + largCent + " cm<br>" +
+	"Altura: " + heightCent + " cm<br>" +
+	"Diagonal: " + diagCent + " cm<br>";
+}
